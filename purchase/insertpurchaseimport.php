122d933629cfeb5ad0d2e9cc135ac05d7cb96ce2
Laurentinus Heriyanto Setiadi <laurentinus@student.umn.ac.id>
2024-02-27 12:28:28 +0700

    Heri 26 february 2024

---
+++ purchase/insertpurchaseimport.php
@@ -0,0 +1,71 @@
+<?php
+//Header access is required
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+//Display error message
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+//Connection access
+require_once('../connection/connection.php');
+
+//Checking call API method
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+    $purchase_order_number = $_POST['purchase_order_number'];
+    $purchase_order_date = $_POST['purchase_order_date'];
+    $purchase_order_supplier = $_POST['purchase_order_supplier'];
+    $purchase_order_shipment = $_POST['purchase_order_shipment'];
+    $purchase_order_term = $_POST['purchase_order_term'];
+    $purchase_order_payment = $_POST['purchase_order_payment'];
+    $purchase_order_origin = $_POST['purchase_order_origin'];
+    $purchase_order_shippingmarks = $_POST['purchase_order_shippingmarks'];
+    $purchase_order_remarks = $_POST['purchase_order_remarks'];
+    $purchase_order_status = $_POST['purchase_order_status'];
+    $purchase_order_type = $_POST['purchase_order_type'];
+    $purchase_order_currency = $_POST['purchase_order_currency'];
+    $insert_by = $_POST['insert_by'];
+
+    $currentDateTime = new DateTime();
+    $indonesiaTimeZone = new DateTimeZone('Asia/Jakarta');
+    $currentDateTime->setTimezone($indonesiaTimeZone);
+    $currentDateTimeString = $currentDateTime->format("Y-m-d H:i:s");
+
+    $insert_purchase_import_query = "INSERT IGNORE INTO purchaseOrder (PONumber, PODate, POSupplier, POShipment, POTerm, POPayment, POOrigin, POShippingMarks, PORemarks, POStatus, POtype, POCurrency, InsertBy, InsertDt) VALUES ('$purchase_order_number','$purchase_order_date', '$purchase_order_supplier', '$purchase_order_shipment', '$purchase_order_term', '$purchase_order_payment','$purchase_order_origin','$purchase_order_shippingmarks','$purchase_order_remarks', '$purchase_order_status', '$purchase_order_type', '$purchase_order_currency', '$insert_by', '$currentDateTimeString');";
+    
+    if(mysqli_query($connect, $insert_purchase_import_query)){
+        
+        http_response_code(200);
+        echo json_encode(
+            array(
+                "StatusCode" => 200,
+                'Status' => 'Success',
+                "message" => "Success: Data inserted successfully"
+            )
+        );
+
+    } else {
+        http_response_code(500);
+        echo json_encode(
+            array(
+                "StatusCode" => 500,
+                'Status' => 'Error',
+                "message" => "Error: Unable to insert data to purchaseOrder table - " . mysqli_error($connect)
+            )
+        );
+    }
+
+} else {
+    http_response_code(404);
+    echo json_encode(
+        array(
+            "StatusCode" => 404,
+            'Status' => 'Error',
+            "message" => "Error: API not found"
+        )
+    );
+}
+
+?>
